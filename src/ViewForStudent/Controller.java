5a1bdb58e4470ae6f5e8070d3ec59f13ca1db5d0
saad zubairi <saadhzubairi@gmail.com>
2020-12-22 19:22:20 +0500

    Implemented Location Based Table addition of students and tutors

--- src/ViewForStudent/Controller.java
+++ src/ViewForStudent/Controller.java
@@ -36,11 +36,22 @@ public class Controller implements Initializable {
     ObservableList<Tutor> tuts = FXCollections.observableArrayList();
     Student stu = new Student();
     
+    String[][] map = {
+           
+           { "Gulshan", "Jauhar", "NIPA", "University Rd." },
+           { "Shahrah-e-Faisal", "Hill-top", "Safoora", "Korangi" },
+           { "Defence", "Hijri", "Gizri", "New Town" },
+           { "Tariq Road", "Gulberg", "FB. Area", "Nazimabad" }
+        
+    };
+    String[] prefloc = new String[0];
+    
     public void listinjector(ArrayList Tuts, ArrayList Studs, Student stu) {
         this.Tuts = Tuts;
         this.Studs = Studs;
         this.stu = stu;
-        adder(this.Tuts,this.stu,this.tuts);
+        adder(this.Tuts,this.stu,this.tuts,this.prefloc);
+        String[] prefloc = (changer(map, stu.getLocation()));
     }
     
     @Override
@@ -97,13 +108,79 @@ public class Controller implements Initializable {
         
     }
     
-    public static void adder(ArrayList<Tutor> Tuts,Student stu, ObservableList<Tutor> tuts){
+    public static void adder(ArrayList<Tutor> Tuts,Student stu, ObservableList<Tutor> tuts, String[] prefloc){
         int[] x = stu.getClass_code();
         for (Tutor t : Tuts) {
             boolean[][] bool = t.getClasses();
             if(bool[x[0]][x[1]]){
-                tuts.add(t);
+                for (int i = 0; i < prefloc.length; i++) {
+                    if(prefloc[i].equals(t.getLocation()))
+                    tuts.add(t);
+                    break;
+                }
             }
         }
+    }
+    public static String[] changer(String[][] x, String in) {
+        String[] out = new String[0];
+        
+        int size = x.length - 1;
+        int I = 0;
+        int J = 0;
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x.length; j++) {
+                if (in.equals(x[i][j])) {
+                    I = i;
+                    J = j;
+                    break;
+                }
+            }
+        }
+        
+        /*Corner Case*/
+        if (I == 0 && J == 0) {
+            out = new String[]{ in, x[I + 1][J], x[I][J + 1], x[I + 1][J + 1] };
+            return out;
+        }
+        
+        else if (I == size && J == 0) {
+            out = new String[]{ x[I][J], x[I - 1][J], x[I][J + 1], x[I - 1][J + 1] };
+            return out;
+        }
+        
+        else if (I == 0 && J == size) {
+            out = new String[]{ x[I][J], x[I + 1][J], x[I + 1][J - 1], x[I][J - 1] };
+            return out;
+        }
+        
+        else if (I == size && J == size) {
+            out = new String[]{ x[I][J], x[I - 1][J], x[I][J - 1], x[I - 1][J - 1] };
+            return out;
+        }
+        
+        if (I == 0) {
+            out = new String[]{ x[I][J], x[I + 1][J + 1], x[I + 1][J - 1], x[I + 1][J], x[I][J + 1], x[I][J - 1] };
+            return out;
+        }
+        if (J == 0) {
+            out = new String[]{ x[I][J], x[I + 1][J], x[I - 1][J], x[I][J + 1], x[I - 1][J + 1], x[I + 1][J + 1] };
+            return out;
+        }
+        if (I == size) {
+            out = new String[]{ x[I][J], x[I][J - 1], x[I][J + 1], x[I - 1][J], x[I - 1][J - 1], x[I - 1][J + 1], };
+            return out;
+        }
+        if (J == size) {
+            out = new String[]{ x[I][J], x[I + 1][J], x[I - 1][J], x[I][J - 1], x[I + 1][J - 1], x[I - 1][J - 1], };
+            return out;
+        }
+        
+        else{
+            out = out = new String[]{ x[I-1][J-1],    x[I-1][J],    x[I-1][J+1],
+                   x[I]  [J-1],    x[I]  [J],    x[I]  [J+1],
+                   x[I+1][J-1],    x[I+1][J],    x[I+1][J+1], };
+        }
+        
+        return out;
     }
 }
--- src/ViewForTutor/Controller.java
+++ src/ViewForTutor/Controller.java
@@ -42,12 +42,24 @@ public class Controller implements Initializable {
     String[] app_locs = new String[9];
     Tutor tu = new Tutor();
     
+    String[][] map = {
+           
+           { "Gulshan", "Jauhar", "NIPA", "University Rd." },
+           { "Shahrah-e-Faisal", "Hill-top", "Safoora", "Korangi" },
+           { "Defence", "Hijri", "Gizri", "New Town" },
+           { "Tariq Road", "Gulberg", "FB. Area", "Nazimabad" }
+        
+    };
+    
+    String[] prefloc = new String[0];
+    
     
     public void listinjector(ArrayList Tuts, ArrayList Studs, Tutor tu) {
         this.Tuts = Tuts;
         this.Studs = Studs;
         this.tu = tu;
-        studs.addAll(Studs);
+        String[] prefloc = (changer(map, tu.getLocation()));
+        adder(this.tu,Studs,studs,prefloc);
     }
     
     @Override
@@ -107,15 +119,81 @@ public class Controller implements Initializable {
         });
     }
     
-    public static void adder(Tutor tu, ArrayList<Student> Studs, ObservableList<Student> studs){
+    public static void adder(Tutor tu, ArrayList<Student> Studs, ObservableList<Student> studs, String[] prefloc){
         boolean[][] bool = tu.getClasses();
         for (Student stu: Studs) {
             int[] code = stu.getClass_code();
             int i = code[0];
             int j = code[1];
             if(bool[i][j]){
-                studs.add(stu);
+                for (int k = 0; k < prefloc.length; k++) {
+                    if(prefloc[k].equals(stu.getLocation()))
+                    studs.add(stu);
+                    break;
+                }
             }
         }
+    }
+    public static String[] changer(String[][] x, String in) {
+        String[] out = new String[0];
+        
+        int size = x.length - 1;
+        int I = 0;
+        int J = 0;
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x.length; j++) {
+                if (in.equals(x[i][j])) {
+                    I = i;
+                    J = j;
+                    break;
+                }
+            }
+        }
+        
+        /*Corner Case*/
+        if (I == 0 && J == 0) {
+            out = new String[]{ in, x[I + 1][J], x[I][J + 1], x[I + 1][J + 1] };
+            return out;
+        }
+        
+        else if (I == size && J == 0) {
+            out = new String[]{ x[I][J], x[I - 1][J], x[I][J + 1], x[I - 1][J + 1] };
+            return out;
+        }
+        
+        else if (I == 0 && J == size) {
+            out = new String[]{ x[I][J], x[I + 1][J], x[I + 1][J - 1], x[I][J - 1] };
+            return out;
+        }
+        
+        else if (I == size && J == size) {
+            out = new String[]{ x[I][J], x[I - 1][J], x[I][J - 1], x[I - 1][J - 1] };
+            return out;
+        }
+        
+        if (I == 0) {
+            out = new String[]{ x[I][J], x[I + 1][J + 1], x[I + 1][J - 1], x[I + 1][J], x[I][J + 1], x[I][J - 1] };
+            return out;
+        }
+        if (J == 0) {
+            out = new String[]{ x[I][J], x[I + 1][J], x[I - 1][J], x[I][J + 1], x[I - 1][J + 1], x[I + 1][J + 1] };
+            return out;
+        }
+        if (I == size) {
+            out = new String[]{ x[I][J], x[I][J - 1], x[I][J + 1], x[I - 1][J], x[I - 1][J - 1], x[I - 1][J + 1], };
+            return out;
+        }
+        if (J == size) {
+            out = new String[]{ x[I][J], x[I + 1][J], x[I - 1][J], x[I][J - 1], x[I + 1][J - 1], x[I - 1][J - 1], };
+            return out;
+        }
+        
+        else{
+            out = out = new String[]{ x[I-1][J-1],    x[I-1][J],    x[I-1][J+1],
+                   x[I]  [J-1],    x[I]  [J],    x[I]  [J+1],
+                   x[I+1][J-1],    x[I+1][J],    x[I+1][J+1], };
+        }
+        
+        return out;
     }
 }
